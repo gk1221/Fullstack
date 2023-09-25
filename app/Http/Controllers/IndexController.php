b1a5f5bacc314830a56effcd37481c2ff8f91f9e
gk1221 <good125628@gmail.com>
2023-09-25 23:28:01 +0800

    list query

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -9,6 +10,11 @@ class IndexController extends Controller
     //
     public function index()
     {
+        dd(
+            Listing::where('beds', '>=', 4)
+                ->orderBy('price', 'ASC')
+                ->first()
+        );
         return inertia('Index/Index', ['message' => 'Hello from Laravel!']);
     }
     public function show()
