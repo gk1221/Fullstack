bcb7f0e9281688729141e348cc3ff7e3c180232d
chu <good125628@gmail.com>
2024-01-14 16:19:58 +0800

    image vue set

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -1,14 +1,61 @@
 <template>
-    <form
-        method="POST"
-        enctype="multipart/form-data"
-        :action="route('realtor.listing.image.store', { listing: listing.id })"
-    >
-        <input type="file" multiple name="images[]" />
-        <button type="submit">Send</button>
-    </form>
+    <Box>
+        <template #header>Upload New Images</template>
+        <form @submit.prevent="upload">
+            <section class="flex items-center gap-2 my-4">
+                <input
+                    type="file"
+                    multiple
+                    @input="addFiles"
+                    class="border rounded-md file:px-4 file:py-2 border-gray-200 dark:border-gray-700 file:text-gray-700 file:dark:text-gray-400 file:border-0 file:bg-gray-100 file:dark:bg-gray-800 file:font-medium file:hover:bg-gray-200 file:dark:hover:bg-gray-700 file:hover:cursor-pointer file:mr-4"
+                />
+                <button
+                    type="submit"
+                    class="btn-outline disabled:opacity-25 disabled:cursor-not-allowed"
+                    :disabled="!canUpload"
+                >
+                    Upload
+                </button>
+                <button type="submit" class="btn-outline" @click="reset">
+                    Reset
+                </button>
+            </section>
+        </form>
+    </Box>
 </template>
 
 <script setup>
-defineProps({ listing: Object });
+import Box from "@/Components/UI/Box.vue";
+import { useForm } from "@inertiajs/vue3";
+import { Inertia } from "@inertiajs/inertia";
+import NProgress from "nprogress";
+import { computed } from "vue";
+
+const props = defineProps({ listing: Object });
+Inertia.on("progress", (event) => {
+    if (event.detail.progress.percentage) {
+        NProgress.set((event.detail.progress.percentage / 100) * 0.9);
+    }
+});
+
+const form = useForm({
+    images: [],
+});
+
+const canUpload = computed(() => form.images.length);
+const upload = () => {
+    form.post(
+        route("realtor.listing.image.store", { listing: props.listing.id }),
+        {
+            onSuccess: () => form.reset("images"),
+        }
+    );
+};
+
+const addFiles = (event) => {
+    for (const image of event.target.files) {
+        form.images.push(image);
+    }
+};
+const reset = () => form.reset("images");
 </script>
